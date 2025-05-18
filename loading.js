8c3084210a9b5a970a00384a989ba0268b2a29aa
Joshua Placiego <placiegojoshua@gmail.com>
2025-05-18 23:58:05 +0800

    upgrade1

---
+++ loading.js
@@ -0,0 +1,7 @@
+function showLoading() {
+  document.getElementById('loadingOverlay').classList.add('active');
+}
+
+function hideLoading() {
+  document.getElementById('loadingOverlay').classList.remove('active');
+}
